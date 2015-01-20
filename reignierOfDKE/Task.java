3ca4adbacef52136e3c9ceb4771a0d78fd567ea3
fabianAtUm <fb.braun@student.maastrichtuniversity.nl>
2015-01-20 14:07:38 +0100

    added javadoc. If this causes an error I will go home and never come back...

--- reignierOfDKE/Task.java
+++ reignierOfDKE/Task.java
@@ -1,6 +1,37 @@
 package reignierOfDKE;
 
+import battlecode.common.RobotType;
+
 public enum Task {
-	BUILD_PASTR, BUILD_NOISETOWER, GOTO, ACCUMULATE, CIRCULATE;
+
+	/**
+	 * Move to the target.<br\>
+	 * Construct a {@link RobotType}.PASTR, as soon as the target is reached
+	 */
+	BUILD_PASTR,
+
+	/**
+	 * Move to the target using a complete pathfinding algorithm.<br\>
+	 * When adjacent to the target build a {@link RobotType}.NOISETOWER
+	 */
+	BUILD_NOISETOWER,
+
+	/**
+	 * Move to the target using a complete pathfinding algorithm.<br\>
+	 */
+	GOTO,
+
+	/**
+	 * Move to the target using a greedy pathfinding algorithm.<br\>
+	 * The target must not be reached, moving in the direction of it is enough.
+	 */
+	ACCUMULATE,
+
+	/**
+	 * Move to the target using a complete pathfinding algorithm.<br\>
+	 * When in a certain range around the target do random moves. When too close
+	 * to target move away from it using a greedy path finding algorithm.
+	 */
+	CIRCULATE;
 
 }
